8351e0740d8a0b670385676580b2742eb859240d
Hugo Rodger-Brown <hugo@yunojuno.com>
2023-09-08 17:42:01 +0100

    Force sort order on models and fields

--- anonymiser/management/commands/display_model_anonymisation.py
+++ anonymiser/management/commands/display_model_anonymisation.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from django.apps import apps
 from django.core.management.base import BaseCommand
+from django.db.models import ForeignObjectRel, Model
 from django.template.loader import render_to_string
 
 from anonymiser.models import FieldSummaryData
@@ -10,17 +11,32 @@ from anonymiser.registry import get_model_anonymiser
 
 
 class Command(BaseCommand):
+    def get_models(self) -> list[type[Model]]:
+        """Force alphabetical order of models."""
+        return sorted(apps.get_models(), key=lambda m: m._meta.label)
+
+    def get_fields(self, model: type[Model]) -> list:
+        """Get model fields ordered by type and then name."""
+        return sorted(
+            [
+                f
+                for f in model._meta.get_fields()
+                if not isinstance(f, ForeignObjectRel)
+            ],
+            key=lambda f: f.__class__.__name__ + f.name,
+        )
+
     def handle(self, *args: Any, **options: Any) -> None:
-        models = defaultdict(list)
-        for model in apps.get_models():
+        context_models = defaultdict(list)
+        for model in self.get_models():
             anonymiser = get_model_anonymiser(model)
-            for f in model._meta.get_fields():
-                is_anonymisable = (
-                    anonymiser.is_field_anonymisable(f.name) if anonymiser else False
-                )
+            for f in self.get_fields(model):
+                is_anonymisable = False
+                if anonymiser:
+                    is_anonymisable = anonymiser.is_field_anonymisable(f.name)
                 field_data = FieldSummaryData(f, is_anonymisable)
-                models[field_data.model_label].append(field_data)
+                context_models[field_data.model_label].append(field_data)
         out = render_to_string(
-            "display_model_anonymisation.md", {"models": dict(models)}
+            "display_model_anonymisation.md", {"models": dict(context_models)}
         )
         self.stdout.write(out)
